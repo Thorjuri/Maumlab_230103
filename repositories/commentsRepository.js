1d5bb6e729e9693bf9d9c3baa608d11215fbc9de
Thorjuri <sjl1992@naver.com>
2023-01-05 00:16:06 +0900

    1/5 comments 코멘트 관련 API 구현 완료. DB  모델 관계설정 완료

--- repositories/commentsRepository.js
+++ repositories/commentsRepository.js
@@ -1,7 +1,43 @@
-const { Users, Posts, Comments } = require('../models');
+const { Users, Posts, Comments, Replies } = require('../models');
 
 class CommentsRepository {
 
+    checkPost = async(postId)=> {
+        const data = await Posts.findOne({ where: { postId } });
+        return data;
+    };
+    
+    createCmt = async(nickname, postId, comment)=> {
+        const data = await Comments.create({ nickname, postId, comment });
+        return data;
+    };
+    
+    getAllCmts = async(postId)=> {
+        const data = await Comments.findAll({
+            raw: true,
+            where: { postId }
+        });
+        return data;
+    };
+    
+    checkCmt = async(commentId)=> {
+        const data = await Comments.findOne({ where: { commentId } });
+        return data;
+    };
+    
+    createReply = async(nickname, postId, commentId, reply)=> {
+        const data = await Replies.create({ nickname, postId, commentId, reply });
+        return data;
+    };
+
+    getAllReply = async(commentId)=> {
+        const data = await Replies.findAll({ 
+            raw: true,
+            where : { commentId },
+            attributes : ['reply']
+        });
+        return data;
+    };
 };
 
 module.exports = CommentsRepository;
--- services/commentsService.js
+++ services/commentsService.js
@@ -2,7 +2,67 @@ const CommentsRepository = require('../repositories/commentsRepository');
 
 class CommentsService {
     commentsRepository = new CommentsRepository();
+    err = new Error('CommentsService Error!!');
 
+    createCmt = async(nickname, postId, comment)=> {
+        const post = await this.commentsRepository.checkPost(postId);
+        if(!post){
+            this.err.status = 400;
+            this.err.message = '해당 게시글이 존재하지 않습니다.';
+            throw this.err;
+        };
+        if(!comment){
+            this.err.status = 400;
+            this.err.message = '댓글 내용을 입력해주세요.';
+            throw this.err;
+        };
+        if(comment.length > 70){
+            this.err.status = 400;
+            this.err.message = '댓글은 70자 이하로 작성해주세요.';
+            throw this.err;
+        };
+
+        const data = await this.commentsRepository.createCmt(nickname, postId, comment);
+        return { data, message: "댓글 작성 완료"};
+    };
+
+    getAllCmts = async(postId)=> {
+        const comments = await this.commentsRepository.getAllCmts(postId);
+        const result = await Promise.all(comments.map(async(val)=> {
+            const replies = await this.commentsRepository.getAllReply(val.commentId);
+            const reply = replies.map((val)=> {
+                return val.reply;
+            });
+            return { commentId: val.commentId, comment:val.comment, reply: reply };
+        }));
+        if(result.length === 0){
+            return { message: "해당 게시글에 댓글이 존재하지 않습니다."}
+        }else{
+            return result;
+        };
+    };
+
+    createReply = async(nickname, postId, commentId, reply)=> {
+        const post = await this.commentsRepository.checkPost(postId);
+        const comment = await this.commentsRepository.checkCmt(commentId);
+        if(!post){
+            this.err.status = 400;
+            this.err.message = '해당 게시글이 존재하지 않습니다.';
+            throw this.err;
+        };
+        if(!comment){
+            this.err.status = 400;
+            this.err.message = '기존 댓글이 존재하지 않습니다.';
+            throw this.err;
+        };
+        if(!reply || reply.length > 50){
+            this.err.status = 400;
+            this.err.message = '대댓글을 50자 이하로 작성해주세요';
+            throw this.err;
+        };
+        const data = await this.commentsRepository.createReply(nickname, postId, commentId, reply);
+        return { data, message: "대댓글 작성 완료"};
+    };
 };
 
 module.exports = CommentsService;
